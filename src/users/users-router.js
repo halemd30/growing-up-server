c2caf32add2876b6f0edb6fe50767d8bfa5e7f60
Mac Hale <machaledesign@gmail.com>
2020-07-24 00:22:47 -0600

    completes child endpoint

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const UsersService = require("./users-service");
 const path = require("path");
+const UsersService = require("./users-service");
 
 const usersRouter = express.Router();
 
