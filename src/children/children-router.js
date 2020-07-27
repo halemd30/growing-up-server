f83f6ebba480783a19f7cc1c0d2ab92ff2cde5c2
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-07-27 16:16:33 -0400

    formatted with prettier to single quotes

--- src/children/children-router.js
+++ src/children/children-router.js
@@ -1,101 +1,98 @@
-require("dotenv").config();
-const express = require("express");
-const path = require("path");
-const ChildrenService = require("./children-service");
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const ChildrenService = require('./children-service');
 const jsonParser = express.json();
-const { requireAuth } = require("../middleware/jwt-auth");
+const { requireAuth } = require('../middleware/jwt-auth');
 
 const childrenRouter = express.Router();
 
 childrenRouter
-  .route("/")
-  .all(requireAuth)
-  .get((req, res, next) => {
-    const db = req.app.get("db");
-    const user_id = req.user.id;
+    .route('/')
+    .all(requireAuth)
+    .get((req, res, next) => {
+        const db = req.app.get('db');
+        const user_id = req.user.id;
 
-    ChildrenService.getByUserId(db, user_id)
-      .then((children) => {
-        res.json(children.map(ChildrenService.serializeChildren));
-      })
-      .catch(next);
-  })
-  .post(jsonParser, (req, res, next) => {
-    const db = req.app.get("db");
+        ChildrenService.getByUserId(db, user_id)
+            .then(children => {
+                res.json(children.map(ChildrenService.serializeChildren));
+            })
+            .catch(next);
+    })
+    .post(jsonParser, (req, res, next) => {
+        const db = req.app.get('db');
 
-    const { first_name, age } = req.body;
-    const newChildren = {
-      first_name,
-      age,
-      user_id: req.user.id,
-    };
-    for (const [key, value] of Object.entries(newChildren))
-      if (value == null)
-        return res.status(400).json({
-          error: { message: `Missing '${key}' in request body` },
-        });
+        const { first_name, age } = req.body;
+        const newChildren = {
+            first_name,
+            age,
+            user_id: req.user.id,
+        };
+        for (const [key, value] of Object.entries(newChildren))
+            if (value == null)
+                return res.status(400).json({
+                    error: { message: `Missing '${key}' in request body` },
+                });
 
-    ChildrenService.insertChildren(db, newChildren)
-      .then((child) => {
-        res
-          .status(201)
-          .location(path.posix.join(req.originalUrl, `/${child.id}`))
-          .json(ChildrenService.serializeChildren(child));
-      })
-      .catch(next);
-  });
+        ChildrenService.insertChildren(db, newChildren)
+            .then(child => {
+                res.status(201)
+                    .location(path.posix.join(req.originalUrl, `/${child.id}`))
+                    .json(ChildrenService.serializeChildren(child));
+            })
+            .catch(next);
+    });
 
 childrenRouter
-  .route("/:childId")
-  .all(requireAuth, jsonParser, (req, res, next) => {
-    const db = req.app.get("db");
-    const child_id = req.params.childId;
+    .route('/:childId')
+    .all(requireAuth, jsonParser, (req, res, next) => {
+        const db = req.app.get('db');
+        const child_id = req.params.childId;
 
-    ChildrenService.getById(db, child_id)
-      .then((child) => {
-        if (!child) {
-          return res.status(404).json({
-            error: { message: "Child does not exist" },
-          });
+        ChildrenService.getById(db, child_id)
+            .then(child => {
+                if (!child) {
+                    return res.status(404).json({
+                        error: { message: 'Child does not exist' },
+                    });
+                }
+                res.child = child;
+                next();
+            })
+            .catch(next);
+    })
+    .get((req, res, next) => {
+        res.json(ChildrenService.serializeChildren(res.child));
+    })
+    .delete((req, res, next) => {
+        const db = req.app.get('db');
+
+        const id = req.params.childId;
+        ChildrenService.deleteChildren(db, id).then(res.status(204).end()).catch(next);
+    })
+    .patch((req, res, next) => {
+        const db = req.app.get('db');
+        const { first_name, age } = req.body;
+        const child_id = req.params.childId;
+
+        const updatedChildren = {
+            first_name,
+            age,
+        };
+
+        const values = Object.values(updatedChildren).filter(Boolean).length;
+        if (values === 0) {
+            return res.status(400).json({
+                error: { message: `Request body must contain value to update` },
+            });
         }
-        res.child = child;
-        next();
-      })
-      .catch(next);
-  })
-  .get((req, res, next) => {
-    res.json(ChildrenService.serializeChildren(res.child));
-  })
-  .delete((req, res, next) => {
-    const db = req.app.get("db");
 
-    const id = req.params.childId;
-    ChildrenService.deleteChildren(db, id)
-      .then(res.status(204).end())
-      .catch(next);
-  })
-  .patch((req, res, next) => {
-    const db = req.app.get("db");
-    const { first_name, age } = req.body;
-    const child_id = req.params.childId;
-
-    const updatedChildren = {
-      first_name,
-      age,
-    };
-
-    const values = Object.values(updatedChildren).filter(Boolean).length;
-    if (values === 0) {
-      return res.status(400).json({
-        error: { message: `Request body must contain value to update` },
-      });
-    }
-
-    ChildrenService.updateChildren(db, child_id, updatedChildren)
-      .then((child) => {
-        res.status(201).json(ChildrenService.serializeChildren(child));
-      })
-      .catch(next);
-  });
+        ChildrenService.updateChildren(db, child_id, updatedChildren)
+            .then(child => {
+                res.status(201).json(ChildrenService.serializeChildren(child));
+            })
+            .catch(next);
+    });
 
 module.exports = childrenRouter;
