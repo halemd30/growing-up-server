a37f4a3aaf0fc13d42416b313a858b1882a10f08
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-08-11 22:48:11 -0400

    finished cascade on delete

--- migrations/006.undo.drop_foreign_key.sql
+++ migrations/006.undo.drop_foreign_key.sql
@@ -2,10 +2,10 @@ BEGIN;
 
 ALTER TABLE eating 
 DROP CONSTRAINT child_id,
-ADD CONSTRAINT child_id FOREIGN KEY (child_id) REFERENCES children(id);
+ADD CONSTRAINT eating_child_id_fkey FOREIGN KEY (child_id) REFERENCES children(id);
 
 ALTER TABLE sleeping 
 DROP CONSTRAINT child_id,
-ADD CONSTRAINT child_id FOREIGN KEY (child_id) REFERENCES children(id);
+ADD CONSTRAINT sleeping_child_id_fkey FOREIGN KEY (child_id) REFERENCES children(id);
 
 COMMIT;
