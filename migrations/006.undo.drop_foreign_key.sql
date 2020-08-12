8ade09d616c4a55bc797c01ceef5fc8bf84ed732
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-08-11 22:26:00 -0400

    removed integer, added foreign key

--- migrations/006.undo.drop_foreign_key.sql
+++ migrations/006.undo.drop_foreign_key.sql
@@ -2,10 +2,10 @@ BEGIN;
 
 ALTER TABLE eating 
 DROP CONSTRAINT child_id,
-ADD CONSTRAINT child_id INTEGER REFERENCES children(id);
+ADD CONSTRAINT child_id REFERENCES children(id);
 
 ALTER TABLE sleeping 
 DROP CONSTRAINT child_id,
-ADD CONSTRAINT child_id INTEGER REFERENCES children(id);
+ADD CONSTRAINT child_id REFERENCES children(id);
 
 COMMIT;
