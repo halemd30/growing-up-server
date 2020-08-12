5113519c94285781e195611b2ce9c3ef30eabfe2
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-08-11 22:23:25 -0400

    fixing foreign key

---
+++ migrations/006.undo.drop_foreign_key.sql
@@ -0,0 +1,11 @@
+BEGIN;
+
+ALTER TABLE eating 
+DROP CONSTRAINT child_id
+ADD CONSTRAINT child_id INTEGER REFERENCES children(id);
+
+ALTER TABLE sleeping 
+DROP CONSTRAINT child_id
+ADD CONSTRAINT child_id INTEGER REFERENCES children(id);
+
+COMMIT;
