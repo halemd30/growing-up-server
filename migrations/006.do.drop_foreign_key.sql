a598c16d60e6b4ccffc3ccff3285192947f8c6ec
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-08-11 22:37:36 -0400

    changes constraint name

--- migrations/006.do.drop_foreign_key.sql
+++ migrations/006.do.drop_foreign_key.sql
@@ -1,11 +1,11 @@
 BEGIN;
 
 ALTER TABLE eating 
-DROP CONSTRAINT child_id,
+DROP CONSTRAINT eating_child_id_fkey,
 ADD CONSTRAINT child_id FOREIGN KEY (child_id) REFERENCES children(id) ON DELETE CASCADE;
 
 ALTER TABLE sleeping 
-DROP CONSTRAINT child_id,
+DROP CONSTRAINT sleeping_child_id_fkey,
 ADD CONSTRAINT child_id FOREIGN KEY (child_id) REFERENCES children(id) ON DELETE CASCADE;
 
 COMMIT;
